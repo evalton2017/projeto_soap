53c46c34e7f521bfd1b914dba965e4c3bc053cce
evalton <evalton.nunes@gmail.com>
2020-02-11 18:52:00 -0300

    projeto soap

---
+++ src/main/java/com/soap/projetosoap/ProjetoSoapApplication.java
@@ -0,0 +1,13 @@
+package com.soap.projetosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSoapApplication.class, args);
+	}
+
+}
